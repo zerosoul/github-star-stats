e2b19e479108e64a288d7f174f2f5807e0866f3e
zerosoul <yanggc888@163.com>
2021-11-11 17:24:04 +0800

    chore: remove console codes

--- vite.config.js
+++ vite.config.js
@@ -26,4 +26,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
