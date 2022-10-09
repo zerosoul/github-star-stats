91fcd3b644db1c4c4b1620b8004672a744ef13c0
Tristan Yang <yanggc888@163.com>
2022-10-09 17:03:45 +0800

    chore: remove log

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,9 @@ import GlobalStyle from './Global.style';
 // import { ConfigProvider } from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 // import zhCN from 'antd/es/locale/zh_CN';
-console.log('wtf', import.meta, import.meta.env.VITE_G_TOKEN);
-
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  // 开发环境从根目录 .env.development.local 读取
+  // 根目录 .env 读取 or cli 环境变量
   const token = import.meta.env.VITE_G_TOKEN || '';
   // const token = localStorage.getItem('AUTH_TOKEN');
   // return the headers to the context so httpLink can read them
