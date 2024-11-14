8887d909b22e8c21b8a82d9b27c49307a9575aff
Tristan Yang <yanggc888@163.com>
2024-11-14 23:40:55 +0800

    chore: updates

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as styleImport from 'vite-plugin-style-import'
+import * as styleImport from 'vite-plugin-style-import';
 import pkg from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: (new URL(pkg.homepage)).pathname,
+  base: new URL(pkg.homepage).pathname,
+  preview: {
+    port: 3000,
+    host: true
+  },
   server: {
     port: 3009
   },
-  plugins: [styleImport.createStyleImportPlugin({
-    libs: [
-      {
-        libraryName: 'antd',
-        esModule: true,
-        resolveStyle: (name) => {
-          return `antd/es/${name}/style/index`
-        },
-      },
-    ],
-  }), react()],
+  plugins: [
+    styleImport.createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
+          }
+        }
+      ]
+    }),
+    react()
+  ],
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
+        javascriptEnabled: true
+      }
+    }
+  }
 });
